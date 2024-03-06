e3ab8aff859d2440e52cee6b2ab5ffba656a1ba6
Garrett Austin Hurst <gahurst@eceprog1.ecn.purdue.edu>
2024-03-05 21:29:04 -0500

    benchmark update

--- testing/benchmark.py
+++ testing/benchmark.py
@@ -62,7 +62,7 @@ def run_test(config_file, cwd, log=False):
 
 def main():
     cfg_name = 'testing_config.ini'
-    cwd      = '../src/stop_and_go'
+    cwd      = '../src/designed_protocol'
     cfg_path = os.path.join('../../test_config/', cfg_name)
     
     n = 5
